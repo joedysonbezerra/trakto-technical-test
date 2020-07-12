b9d75e0bd74ec274012c8178f2968d5ca59f9dfa
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-12 13:47:51 -0300

    feat: capture data entry in form signin

--- src/app/components/form/form-signin/form-signin.component.ts
+++ src/app/components/form/form-signin/form-signin.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-signin',
-  templateUrl: './form-signin.component.html',
-  styleUrls: ['./form-signin.component.css']
+  selector: "app-form-signin",
+  templateUrl: "./form-signin.component.html",
+  styleUrls: ["./form-signin.component.css"],
 })
 export class FormSigninComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+    });
   }
 
+  onSubmit() {
+    console.log(this.form);
+  }
 }
--- src/app/components/form/form.module.ts
+++ src/app/components/form/form.module.ts
@@ -5,10 +5,19 @@ import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { FormSignupComponent } from "./form-signup/form-signup.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [FormSigninComponent, FormSignupComponent],
-  imports: [CommonModule, MatInputModule, MatButtonModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatIconModule,
+  ],
   exports: [FormSigninComponent, FormSignupComponent],
 })
 export class FormModule {}
