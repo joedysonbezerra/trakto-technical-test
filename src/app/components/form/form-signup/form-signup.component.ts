5577987cca1239890c30932651847b3cd36930ff
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-12 13:47:59 -0300

    feat:capture data entry in form signup

--- src/app/components/form/form-signup/form-signup.component.ts
+++ src/app/components/form/form-signup/form-signup.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-signup',
-  templateUrl: './form-signup.component.html',
-  styleUrls: ['./form-signup.component.css']
+  selector: "app-form-signup",
+  templateUrl: "./form-signup.component.html",
+  styleUrls: ["./form-signup.component.css"],
 })
 export class FormSignupComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+      name: new FormControl(null),
+    });
   }
-
+  onSubmit() {
+    console.log(this.form);
+  }
 }
